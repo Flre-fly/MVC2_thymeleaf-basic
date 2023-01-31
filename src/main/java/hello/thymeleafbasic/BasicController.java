236b497b5b60e53df5f2da8d76dedd693d8a000a
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2023-01-31 21:41:16 +0900

    escape를 사용하는 예제

---
+++ src/main/java/hello/thymeleafbasic/BasicController.java
@@ -0,0 +1,16 @@
+package hello.thymeleafbasic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello<b> Spring!</b>");
+        return "basic/text-basic";
+    }
+}
